3633526a8340956a2e0b4cc52c621f1635006cf7
Vinícius Mardegan <vinimardegan2006@gmail.com>
2025-10-23 17:09:09 -0300

    feat: add jogador struct

---
+++ estruturas/jogador.h
@@ -0,0 +1,65 @@
+#ifndef JOGADOR_H
+#define JOGADOR_H
+
+#define MAX 50
+
+enum Nacionalidade
+{
+    BRASIL,
+    ARGENTINA,
+    COLOMBIA,
+    VENEZUELA,
+    URUGUAI,
+    PARAGUAI,
+    HOLANDA,
+    ITALIA,
+    FRANCA
+};
+
+enum Time
+{
+    PALMEIRAS,
+    FLAMENGO,
+    CORINTHIANS,
+    SAO_PAULO,
+    FLUMINENSE,
+    BOTAFOGO,
+    MIRASSOL,
+    CRUZEIRO,
+    BRAGANTINO,
+    VITORIA,
+    JUVENTUDE,
+    SPORT,
+    ATLETICO_MINEIRO,
+    VASCO,
+    GREMIO,
+    INTERNACIONAL,
+    BAHIA,
+    FORTALEZA,
+    CEARA,
+    SANTOS
+};
+
+typedef enum {
+    GOL,      // Goleiro
+    LD,       // Lateral Direito
+    ZD,       // Zagueiro Direito
+    ZE,       // Zagueiro Esquerdo
+    LE,       // Lateral Esquerdo
+    VOL,      // Volante
+    MD,       // Meia Central Direito
+    ME,       // Meia Central Esquerdo
+    PD,       // Ponta Direita
+    CA,       // Centro Avante (Atacante Central)
+    PE        // Ponta Esquerda
+} Posicao;
+
+typedef struct jogador
+{
+    char nome_jogador[MAX];
+    Time time;
+    Nacionalidade nacionalidade;
+    Posicao posicao;
+} Jogador;
+
+#endif
